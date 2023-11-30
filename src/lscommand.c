2f1dfb444a7caa218c38a539f0bfac03d7629314
Ines Haas <mhaas@prologue.fr>
2023-11-30 16:47:40 +0100

    update lscommand

---
+++ src/lscommand.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <dirent.h>
+
+int main() {
+    // Ouvrir le répertoire courant
+    DIR *dir = opendir(".");
+
+    if (dir == NULL) {
+        perror("Erreur lors de l'ouverture du répertoire");
+        return 1;
+    }
+
+    // Lire le contenu du répertoire
+    struct dirent *entry;
+
+    while ((entry = readdir(dir)) != NULL) {
+        printf("%s\n", entry->d_name);
+    }
+
+    // Fermer le répertoire
+    closedir(dir);
+
+    return 0;
+}
